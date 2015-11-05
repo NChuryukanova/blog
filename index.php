1721ee52fd484db78928cb81914814166add4716
NChuryukanova <nchuryukanova@mail.ru>
2015-11-05 10:11:32 +0300

    first script on PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    //echo "Hello world!";
+    //echo time();
+
+    function add($param1, $param2) {
+        return $param1 + $param2;
+    }
+
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    echo add ($a, $b);
+
+    echo"<br>";
+    for($i=0;$i<10;$i++){
+        echo $i."<br>";
+    }
+?>
